cfe5be0d1b87aac9e1e7ca2eaac9220dd334b3fb
heric-hh <erhh.015@gmail.com>
2022-11-10 20:34:06 -0600

    Implementando método para insertar al inicio de la lista

--- src/operacionesListaLigada/ListaLigada.java
+++ src/operacionesListaLigada/ListaLigada.java
@@ -5,5 +5,28 @@ package operacionesListaLigada;
  * @author hheri
  */
 public class ListaLigada {
-    Nodo inicio; // Referencia de tipo Nodo. Apunta al primer elemento
+    private Nodo inicio; // Referencia de tipo Nodo. Apunta al primer elemento
+    private int tam; // Indica cuantos nodos contiene la lista.
+    
+    // Constructor de la clase
+    
+    /*
+        Si una lista es creada sin elementos, entonces el inicio debe ser null
+        porque no hay elementos en la lista, además de que el tamaño es cero.
+    */
+    public ListaLigada() {
+        this.inicio = null;
+        tam = 0;
+    }
+    
+    // Métodos de la clase
+    
+    // Regresar el tamaño de la lista
+    public int getTamaño() { return this.tam; } 
+    
+    
+    public void insertarAlInicio( int elemento ) {
+        inicio = new Nodo( elemento, inicio ); // Al crear este objeto, se apunta al inicio de la lista  
+        tam++;
+    }
 }
--- src/operacionesListaLigada/Nodo.java
+++ src/operacionesListaLigada/Nodo.java
@@ -7,10 +7,19 @@ package operacionesListaLigada;
 public class Nodo {
     int dato;
     Nodo sig;
+
+    // Constructor de la clase
+
     
     public Nodo( int dato ) {
         this.dato = dato;
         this.sig = null;
+    }
+    
+    // Para este constructor, se necesita recibir un dato y el nodo siguiente
+    public Nodo ( int dato, Nodo sig ) {
+        this.dato = dato;
+        this.sig = sig;
     }
     
     
